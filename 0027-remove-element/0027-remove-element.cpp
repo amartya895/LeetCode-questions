97b263315c57980d0e60b75da7256d84e8ba37b2
Amartya Sen <83586351+amartya895@users.noreply.github.com>
2023-10-02 21:34:14 +0530

    Time: 2 ms (44.37%), Space: 9.1 MB (20.01%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        //4722546259112960
-        //09/23
+        int index = 0;
 
-        vector<int> temp;
-
-        for(int i = 0 ; i < nums.size() ; i++)
+        for(int i = 0 ; i <nums.size() ; i++)
         {
             if(nums[i] != val)
             {
-                temp.push_back(nums[i]);
+                nums[index] = nums[i];
+                index++;
             }
-
-
         }
-
-        for(int i = 0 ; i < temp.size() ; i++)
-        {
-            nums[i] = temp[i];
-        }
-
-        return temp.size();
+        return index;
     }
 };
