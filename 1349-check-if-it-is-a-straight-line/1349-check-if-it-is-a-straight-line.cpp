c74a8c6142cd607c67ed8924dc959d9f155d7d25
Amartya Sen <83586351+amartya895@users.noreply.github.com>
2023-10-05 22:50:59 +0530

    Time: 4 ms (79.15%), Space: 10.5 MB (23.43%) - LeetHub

--- 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.cpp
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.cpp
@@ -1,56 +1,20 @@
 class Solution {
 public:
-    bool checkStraightLine(vector<vector<int>>& coordinates) {
-        int row = coordinates.size();
-        if (row < 2) {
-            // If there are fewer than 2 points, it's considered a straight line.
-            return true;
-        }
-        
-        // Calculate the slope between the first two points.
+    bool checkStraightLine(vector<vector<int>>& coordinates) 
+    {
         int x0 = coordinates[0][0];
         int y0 = coordinates[0][1];
         int x1 = coordinates[1][0];
         int y1 = coordinates[1][1];
-        double slope = (y1 - y0) / (double)(x1 - x0);
-
-        bool isVertical = true;
-        for (int i = 1; i < row; i++) {
-            if (coordinates[i][0] != x0) {
-                isVertical = false;
-                break;
-            }
-        }
-        
-        // Check if all other points have the same y-coordinate (horizontal line).
-        bool isHorizontal = true;
-        for (int i = 1; i < row; i++) {
-            if (coordinates[i][1] != y0) {
-                isHorizontal = false;
-                break;
-            }
-        }
-        
-        if (isVertical || isHorizontal)
+        for (int i = 2; i < coordinates.size(); i++) 
         {
-            return true;
-        }
-        
-        // Check if all other points lie on the same line.
-        for (int i = 2; i < row; i++) {
-            int xi = coordinates[i][0];
-            int yi = coordinates[i][1];
-            
-            // Calculate the slope between the current point and the first point.
-            double currentSlope = (yi - y0) / (double)(xi - x0);
-            
-            // If the slopes are not equal, it's not a straight line.
-            if (currentSlope != slope) {
+            int x = coordinates[i][0];
+            int y = coordinates[i][1];
+            if ((x - x0) * (y1 - y0) != (y - y0) * (x1 - x0))
+             {
                 return false;
             }
         }
-        
-        // If all points are on the same line, it's a straight line.
         return true;
     }
 };
