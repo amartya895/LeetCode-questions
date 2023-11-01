49374f91c41c9b7bdd6ab815a85924e76bddfe3b
Amartya Sen <83586351+amartya895@users.noreply.github.com>
2023-11-01 23:32:27 +0530

    Time: 86 ms (65.3%), Space: 92 MB (35.82%) - LeetHub

--- 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.cpp
+++ 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int findNonMinOrMax(vector<int>& nums) {
-        
-        int n = nums.size();
-        if(n <= 2)
-        {
-            return -1;
-        }
-        sort(nums.begin() , nums.end());
-
-       return nums[1];
-
+        if(nums.size()<=2)return -1;
+        sort(nums.begin(),nums.end());
+        return nums[1];   
     }
 };
