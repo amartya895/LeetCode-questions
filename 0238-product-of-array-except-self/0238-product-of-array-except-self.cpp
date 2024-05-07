2aa992bd95feab084c1ddf9bbc36f621204f16ae
Amartya Sen <83586351+amartya895@users.noreply.github.com>
2024-05-07 23:28:05 +0530

    Time: 20 ms (44.16%), Space: 38.2 MB (13.72%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,53 +1,22 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+         int n = nums.size();
+        vector<int> result(n, 1);
         
-        // int n = nums.size();
-        // vector<int> ans;
-
-        // for(int i = 0 ; i < n ; i++)
-        // {
-        //     int product = 1;
-
-        //     for(int j = 0 ; j < n ; j++)
-        //     {
-        //         if(i == j)
-        //         {
-        //             continue;
-        //         }
-
-        //         product *= nums[j];
-        //     }
-
-        //     ans.push_back(product);
-        // }
-
-        // return ans;
-
-        int n = nums.size();
-        vector<int> ans(n);
-        vector<int> leftPro(n);
-        vector<int> rightPro(n);
-
-        leftPro[0] = 1;
-
-        for(int i = 1 ; i < n ; i++)
-        {
-            leftPro[i] = leftPro[i-1] * nums[i-1];
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = prefix;
+            prefix = prefix * nums[i];
         }
-        rightPro[n-1] = 1;
-
-        for(int i = n-2 ; i>= 0 ; i--)
-        {
-            rightPro[i] = rightPro[i+1]*nums[i+1];
+        
+        int postfix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * postfix;
+            postfix = postfix * nums[i];
         }
-
-        for(int i = 0 ; i < n ; i++)
-        {
-            ans[i] = leftPro[i]*rightPro[i];
-        }
-
-        return ans;
-
+        
+        return result;
+    
     }
 };
